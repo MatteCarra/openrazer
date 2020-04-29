a785491bc46a10046f7da91fea863dd76416fa92
nightsky30 <6024315+nightsky30@users.noreply.github.com>
2020-04-29 06:03:57 -0500

    Re-enable rippple for testing

--- daemon/openrazer_daemon/hardware/keyboards.py
+++ daemon/openrazer_daemon/hardware/keyboards.py
@@ -881,7 +881,7 @@ class RazerCynosaChroma(_RippleKeyboard):
     }
 
 
-class RazerCynosaChromaPro(_MacroKeyboard):
+class RazerCynosaChromaPro(_RippleKeyboard):
     """
     Class for the Razer Cynosa Chroma Pro
     """
@@ -896,7 +896,8 @@ class RazerCynosaChromaPro(_MacroKeyboard):
                'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect',
                'set_custom_effect', 'set_key_row', 'get_game_mode', 'set_game_mode', 'get_macro_mode', 'set_macro_mode',
                'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro',
-               'set_starlight_random_effect', 'set_starlight_single_effect', 'set_starlight_dual_effect']
+               'set_starlight_random_effect', 'set_starlight_single_effect', 'set_starlight_dual_effect',
+               'set_ripple_effect', 'set_ripple_effect_random_colour']
 
     DEVICE_IMAGE = "https://assets.razerzone.com/eeimages/support/products/1257/1257_cynosa_chroma_pro_alt.png"
 
